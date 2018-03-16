80efa8d6755d6e8f3226b45513e76438041f2f4e
imaishunsuke <kbc17b02@stu.kawahara.ac.jp>
2018-03-16 11:57:46 +0900

    test用にtestMirrorクラスを追加

---
+++ Game/testMirror.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "tkEngine/character/tkCharacterController.h"
+
+class testMirror : public IGameObject
+{
+public:
+	testMirror();
+	~testMirror();
+	bool Start();
+	void Update();
+	void Render(CRenderContext& rc);
+	//ÉÅÉìÉoïœêî
+	CSkinModel m_skinModel;
+	CSkinModelData m_skinModelData;
+	CVector3 m_position = CVector3::Zero;
+	CVector3 m_moveSpeed = CVector3::Zero;
+	CQuaternion m_rotation = CQuaternion::Identity;
+	CVector3 m_scale = { 0.2f,0.2f,0.2f };
+	CCharacterController m_charaCon;
+};
+
